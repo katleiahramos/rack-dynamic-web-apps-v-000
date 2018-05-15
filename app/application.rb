3bb515fd4080b692a16bedeaa8d1c98985c4e9b3
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-15 16:00:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,10 @@ class Application
     resp = Rack::Response.new
     resp.write "Hello, World"
     resp.finish
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
   end
 
 end
